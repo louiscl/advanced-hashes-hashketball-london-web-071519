250fa19057990f0d233ee4319bf2fd65b8e55256
Louis Lammerting <louis.lammerting@gmail.com>
2019-07-12 01:52:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,7 +201,8 @@ def player_stats(player_name)
   game_hash.each do |place, team|
     team.each do |attributes, data|
       if attributes == :players
-         data.each do |player| 
+         data.each do |player|
+           
 end
 
 
